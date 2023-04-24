525d8826e61a1e06f41895d92ab3c3bd72d77c8d
xCthulhuFtagn <mkolodyazhny@gmail.com>
2023-04-24 05:37:45 -0700

    minor docker parameters changes

--- CHAT/script.py
+++ CHAT/script.py
@@ -9,13 +9,13 @@ import prometheus_client as prom
 import sys
 import json
 
-# Создаём три метрики
+# Создаём метрики
 good_lines = prom.Counter('webexporter_good_lines', 'Good JSON records')
 wrong_lines = prom.Counter('webexporter_wrong_lines', 'Wrong JSON records')
-errors = prom.Counter('client_read_errors', 'Client read errors')
+read_errors = prom.Counter('client_read_errors', 'Client read errors')
 
-response_time = prom.Histogram('webserver_request_duration', 'Response time', ['client_request_e', 'server_response_e'], 
-    buckets=(.001, .002, .005, .010, .020, .050, .100, .200, .500, float("inf")))
+response_time = prom.Histogram('webserver_request_duration', 'Response time', labelnames=['client_request_e', 'server_response_e'],
+    buckets=(.500, 1.0, 1.5, 2.0, 2.5, 3.0, float("inf")))
 
 # Определим функцию, которую мы будем использовать как main:
 def my_main():
@@ -33,13 +33,10 @@ def my_main():
             # Регистрирум запрос
             if data["message"] == "Read first response from server":
                 total_time_seconds = data["data"]["response time"] / 1000
-                response_time.labels(
-                    client_request_e=data["data"]["response"]["client_request_e"], 
-                    server_response_e=data["data"]["response"]["server_response_e"]
-                ).observe(total_time_seconds)
+                response_time.labels(client_request_e=data["data"]["response"]["client_request_e"], server_response_e=data["data"]["response"]["server_response_e"]).observe(total_time_seconds)
 
             if data["message"] == "Client read error":
-                errors.inc()
+                read_errors.inc()
 
             # Регистрирум успешно разобранную строку
             good_lines.inc()
